bc267885ff88654ed4e9d63a3f81b2cc79811ec8
darkroom51 <darkroom@wp.pl>
2019-05-14 18:50:42 +0200

    resize images

--- src/components/Room.js
+++ src/components/Room.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import defaultImg from "../images/room-1.jpeg";
+import defaultImg from "../images/defaultBcg.jpg";
 import PropTypes from "prop-types";
 import { memo } from "react";
 
--- src/pages/SingleRoom.js
+++ src/pages/SingleRoom.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import defaultBcg from "../images/room-1.jpeg";
+import defaultBcg from "../images/defaultBcg.jpg";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
 import { RoomContext } from "../context";
